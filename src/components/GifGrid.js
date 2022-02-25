303bfd70fa29e28acca98fed6e98cf6975b4bf7a
Alex <alex.hernandez24kb@gmail.com>
2022-02-24 23:58:11 -0600

    Obtener imagenes y bandera de carga

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,28 +1,22 @@
 import React  from 'react';
 import { useFetchGifs } from '../hooks/useFetchGifs';
-//import { GifGridItem } from './GifGridItem';
-//import { getGifs } from '../helpers/getGifs';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
 
-    //const [images, setImages] = useState([]);
-    const { loading } = useFetchGifs();
+    const { data:images, loading } = useFetchGifs( category );
 
 
-    //useEffect( () => {
-    //    getGifs( category )
-    //        .then(setImages);
-    //}, [ category ])
+    
   
 
   return (
        <>
             <h3>{ category }</h3>
 
-            { loading ? 'Cargando...' : 'Data cargada'}
+             {loading && <p>Loading</p>}
 
-
-            {/* <div className="card-grid">
+             <div className="card-grid">
 
                 {
                     images.map( img => (
@@ -34,7 +28,7 @@ export const GifGrid = ({category}) => {
                     ))
                 }
                 
-            </div> */}
+            </div>
        </> 
   )
 }
